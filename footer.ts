005b947ed3921185d522a1443462cd338ce07221
Adriano Ruseler <adrianoruseler@gmail.com>
2026-03-08 14:10:27 -0300

    Update footer links

--- footer.ts
+++ footer.ts
@@ -19,8 +19,136 @@ const footer: ThemeConfig["footer"] = {
     style: "dark",
     links: [
         {
-            title: "Docs",
-            items: [{ label: "Tutorial", to: "/docs/intro" }],
+            title: "UTFPR",
+            items: [
+                {
+                    label: "Moodle",
+                    href: "https://moodle.utfpr.edu.br/",
+                },
+                {
+                    label: "Webmail",
+                    href: "https://webmail.utfpr.edu.br/",
+                },
+                {
+                    label: "Sistemas",
+                    href: "https://sistemas2.utfpr.edu.br/",
+                },
+                {
+                    label: "SEI",
+                    href: "https://sei.utfpr.edu.br/",
+                },
+                {
+                    label: "Chat",
+                    href: "https://chat.utfpr.edu.br/",
+                },
+                {
+                    label: "Ajuda",
+                    href: "https://ajuda.utfpr.edu.br/",
+                },
+                {
+                    label: "Calendário",
+                    href: "https://www.utfpr.edu.br/alunos/calendario",
+                },
+                {
+                    label: "TCC",
+                    href: "https://nupet.daelt.ct.utfpr.edu.br/tcc/engenharia/index.html",
+                },
+                {
+                    label: "Nuvem",
+                    href: "https://nuvem.utfpr.edu.br/",
+                },
+            ],
+        },
+        {
+            title: "Ferramentas",
+            items: [
+                {
+                    label: "CCStudio",
+                    href: "https://www.ti.com/tool/CCSTUDIO",
+                },
+                {
+                    label: "Visual Studio Code",
+                    href: "https://code.visualstudio.com/download",
+                },
+                {
+                    label: "Git SCM",
+                    href: "https://git-scm.com/downloads",
+                },
+                {
+                    label: "GitHub CLI",
+                    href: "https://cli.github.com/",
+                },
+                {
+                    label: "GitHub Desktop",
+                    href: "https://desktop.github.com/download/",
+                },
+                {
+                    label: "WinGet",
+                    href: "https://winget.run/",
+                },
+            ],
+        },
+        {
+            title: "TI",
+            items: [
+                {
+                    label: "TMDSCNCD28379D",
+                    href: "https://www.ti.com/tool/TMDSCNCD28379D",
+                },
+                {
+                    label: "LAUNCHXL-F28379D",
+                    href: "https://www.ti.com/tool/LAUNCHXL-F28379D",
+                },
+                {
+                    label: "LAUNCHXL-F28069M",
+                    href: "https://www.ti.com/tool/LAUNCHXL-F28069M",
+                },
+                {
+                    label: "C2000WARE",
+                    href: "https://www.ti.com/tool/C2000WARE",
+                },
+                {
+                    label: "C28X-ACADEMY",
+                    href: "https://dev.ti.com/tirex/explore/node?isTheia=false&node=A__AEIJm0rwIeU.2P1OBWwlaA__C28X-ACADEMY__1sbHxUB__LATEST",
+                },
+            ],
+        },
+        {
+            title: "AI", //
+            items: [
+                {
+                    label: "Gemini",
+                    href: "https://gemini.google.com/app",
+                }, //
+                {
+                    label: "ChatGPT",
+                    href: "https://chatgpt.com/",
+                }, //
+                {
+                    label: "Claude",
+                    href: "https://claude.ai/",
+                },
+                {
+                    label: "Copilot",
+                    href: "https://copilot.microsoft.com/",
+                },
+                {
+                    label: "DeepSeek",
+                    href: "https://chat.deepseek.com/",
+                },
+                {
+                    label: "Grok",
+                    href: "https://grok.com/",
+                },
+                {
+                    label: "Kimi",
+                    href: "https://www.kimi.com/en",
+                },
+                {
+                    label: "GLM",
+                    href: "https://chat.z.ai/",
+                },
+            ],
         },
         {
             title: "HIL Interfaces",
